e998322291aceafe96f613332efbea289648ffcc
PrrsnCrVS4sr <96426776+PrrsnCrVS4sr@users.noreply.github.com>
2022-11-26 00:53:09 +0530

    Added sphinx to laod balancer

--- load_balancer.py
+++ load_balancer.py
@@ -4,7 +4,20 @@ import sys
 
 
 class LoadBalancerRoundRobin:
+    """
+    This class is designed for load balancing strategy as a round robin at server.
+    basically, the next message is allocated to the next server.
+    The record is maintained in a database.
+    :param 
+    """
     def __init__(self,n_ports,cursor):
+        """Constructor for LoadBalancerRoundRobin Class. Intializing the number of 
+        ports and cursor
+        :param nports: Number of ports
+        :type ports: int
+        :param cursor: cursor to execute query
+        :type cursor: cursor.cursor
+        """
         
         self.current_port_index = 0
         self.num_ports = n_ports
@@ -12,22 +25,39 @@ class LoadBalancerRoundRobin:
         self.create_tables()
 
     def get_port_index(self):
+        """Getting the next port index according to round robin by taking modulus
+        using the get_port_index_from_table(self) index.
+        
+        :param self.current_port_index: port number
+        :type self.current_port_index: int
+        """
         self.current_port_index = (self.get_port_index_from_table())%self.num_ports
         self.update_port_index()
         return self.current_port_index
 
     def update_port_index(self):
+        """
+        Updating the database for the port number used by update query.
+        """
         update =f'''UPDATE RoundRobin SET index = index+1 WHERE id = (1234)'''
         self.cursor.execute(update)
 
     def get_port_index_from_table(self):
+        """
+        Gets the next port number by database through Select query.
+        
+        :param index: index
+        :rtype index: int
+        """
         get =f'''SELECT  index from RoundRobin WHERE id =(1234)'''
         self.cursor.execute(get)
         index = self.cursor.fetchone()[0]
         return index
         
     def create_tables(self) :
-        
+        """
+        Create Table for holding the turns of roundrobin.
+        """
         create ='''
             DROP TABLE IF EXISTS RoundRobin;
             CREATE TABLE IF NOT EXISTS RoundRobin (
@@ -47,11 +77,17 @@ class LoadBalancerRoundRobin:
 
         
 
-        print("Database created successfully........")
+       # print("Database created successfully........")
 
         return
 
 class LoadBalancerCPUUtil:
+    """
+    This class is designed for load balancing strategy according to the load at 
+    cpu which is monitored according to the memory allocated.
+    The least occupied server is given the duty of message.
+    The record is maintained in a database.
+    """
     def __init__(self,n_ports,ports,cursor):
         
         self.ports = ports
@@ -60,12 +96,28 @@ class LoadBalancerCPUUtil:
         self.cursor = cursor
         self.create_tables()
 
+    # def get_port_index(self):
+    #     self.current_port_index = (self.get_port_index_from_table())%self.num_ports
+    #     self.update_port_index()
+    #     return self.current_port_index
+
     def update_port_index(self,cpu_util,port):
+        """Update the port's memory usage at database 
+        :param cpu_util: CPU's memory
+        :type cpu_util: int 
+        :param port: Update the port's memory at cpu
+        :type port: int    
+        """
         update =f'''UPDATE CPUUtil SET util = ({cpu_util}) WHERE port = ({port})'''
         self.cursor.execute(update)
 
     def get_port_from_table(self):
-
+        """
+        Gets the next port number by database through Select query selecting minima
+        of all.
+        :param index: index
+        :rtype index: int
+        """
         min_util_query = '''SELECT Min(util) from CPUUtil'''
         self.cursor.execute(min_util_query)
         min_util = self.cursor.fetchone()[0]
@@ -75,7 +127,9 @@ class LoadBalancerCPUUtil:
         return index ### NOTE: YOU MIGHT HAVE TO do index-1 
         
     def create_tables(self) :
-        
+        """
+        Create Table for holding the cpu's memory usage.
+        """
         create ='''
             CREATE TABLE IF NOT EXISTS CPUUtil (
             id SERIAL,
